0f64a7b8b19614154b376be91b83365b30cdd3eb
chanegamingtv <125740057+chanegamingtv@users.noreply.github.com>
2023-03-25 12:28:59 +0000

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,13 @@
+// Create function addUser()
+
+function addUser() {
+  // Get value of user by id player1_name_input and player2_name_input
+player1=document.getElementById("player1_name_input").value
+player2=document.getElementById("player2_name_input").value
+  // Store these values locally
+localStorage.setItem("player1_name_input", player1)
+localStorage.setItem("player2_name_input", player2)
+  //Assign "game_page.html" to window.location
+  window.location="game_page.html"
+}
+
